02bc1d8ed5a937cce7a4fb66a2950bee49ac3a8d
WildRunner1 <158103980+WildRunner2@users.noreply.github.com>
2024-12-12 23:24:46 +0100

    da

--- src/database.js
+++ src/database.js
@@ -1,26 +1,23 @@
 const { Pool } = require('pg');
 
 // Ensure DATABASE_URL is defined
-if (!process.env.DATABASE_URL) {
+if (!process.env.DATABASE_URL_PROD) {
   console.error('DATABASE_URL environment variable is not set!');
   process.exit(1); // Exit if the environment variable is missing
 }
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL + "?sslmode=require",
+  connectionString: process.env.DATABASE_URL_PROD + "?sslmode=require",
 });
 
-async function connectToDatabase() {
-  try {
-    await pool.connect();  // Use the promise-based connect method
-    console.log("Connected to PostgreSQL successfully!");
-  } catch (err) {
-    console.error('Failed to connect to PostgreSQL:', err.stack);
-    process.exit(1); // Exit the process if connection fails
-  }
-}
+pool.connect((err) => {
+    if (!err){
+    console.log("Connect to PostgreSQL successfully!")}
+    else { console.log(err.message)}
+
+})
 
 // Call the connect function
-connectToDatabase();
+
 
 module.exports = pool;
