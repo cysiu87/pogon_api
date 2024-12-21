5d648505342629fd9adf02c805328baa69395c08
WildRunner1 <158103980+WildRunner2@users.noreply.github.com>
2024-12-21 01:06:47 +0100

    ss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const app = express ();
 const cors = require('cors');
-const getConnection = require('./db-config');
+const basicAuth = require("basic-auth"); 
+
 
 app.use(express.json());
 app.use(cors());
@@ -15,54 +16,24 @@ app.listen(API_PORT, () => {
 const usersRouter = require('./routes/users.router')
 const bddRouter = require('./routes/bdd.router')
 const scriptsRouter = require('./routes/scripts.router')
-  
-app.use("/api/users", usersRouter)
-app.use("/api/bdd", bddRouter)
-app.use("/api/scripts", scriptsRouter)
 
-//DB test!
-// app.get('/test-db', (req, res) => {
-//     const client = getConnection();
-//     var login= req.query.login;
-//     //console.log(login)
-//     var pgsql = "SELECT * from users where login = '" +login +"';";
-   
-//     client.connect((err) => {
-//       if (err) {
-//         console.error("Error connecting to the database:", err.stack);
-//         res.send({ "status": "Error connecting to the database: " + err.stack });
-//         return;
-//       }
-//       client.query(pgsql, (err, results) => {
-//         client.end(); // Close the connection after query
-//         if (err) {
-//           console.error('Error querying the database:', err.stack);
-//           res.send({ "status": "Error querying the database: " + err.stack });
-//           return;
-//         }
-  
-//         // Use 'results.rows' to access the rows returned by PostgreSQL
-//         if (results.rows && results.rows.length > 0) {
-//           //console.table(results.rows)          
-//           res.send(results.rows);
-          
-//         } else {
-//           res.send({ "status": "No rows returned from the query." });
-//         }
-//       });
-//     });
-//   });
-//   app.get("/status", (request, response) => {
-//     const status = {
-//        "Status": "Running"
-//     };
-    
-//     response.send(status);
-//  });
-//  app.get("/", (request, response) => {
-//     const status = {
-//        "Status": "HelloWorld!"
-//     };
-    
-//     response.send(status);
-//  });
+const authenticate = (req, res, next) => {
+  const user = basicAuth(req);
+
+  const validUser =
+    user && user.name === process.env.BASIC_AUTH_USERNAME && user.pass === process.env.BASIC_AUTH_PASSWORD;
+
+  if (validUser) {
+    return next(); // Authentication succeeded
+  } else {
+    res.set("WWW-Authenticate", 'Basic realm="Protected API"');
+    return res.status(401).send("Authentication required.");
+  }
+};
+
+
+
+app.use("/api/users",authenticate, usersRouter)
+app.use("/api/bdd",authenticate, bddRouter)
+app.use("/api/scripts",authenticate, scriptsRouter)
+
